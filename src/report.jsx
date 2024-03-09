6572d04a16dbfa8cad759d38211bb910e56e63bb
OFF <peerapat13th@gmail.com>
2024-03-09 14:43:08 +0700

    Upadte report.jsx

--- src/report.jsx
+++ src/report.jsx
@@ -62,7 +62,7 @@ export default class report extends Component {
         <Navbar bg="primary" variant="dark">
           <Container>
             <Navbar.Brand href="#">ค้นหาเลขไปรษณีย์</Navbar.Brand>
-            <div>{/* Name */}</div>
+            <div>66130151 Peerapat Atta </div>
           </Container>
         </Navbar>
         <Container>
